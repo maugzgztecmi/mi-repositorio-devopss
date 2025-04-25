14dd7a71402f1ed889a774732dc57f0b828cf4cd
mauricio <al02851553@tecmilenio.mx>
2025-04-25 17:22:40 -0600

    xd?

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "¡Hola desde Flask en Vercel!"})
+    return jsonify({"message": "¡Hola desde Flask en Vercel!xdxdxdxdxdx"})
